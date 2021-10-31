a7c3614268e0a6fa59283b715ce9f96b92dd2774
USER <bayzid96@gmail.com>
2021-10-31 22:37:15 +0600

    MuslimDay

--- app/src/main/java/com/example/muslim/RakibActivity.java
+++ app/src/main/java/com/example/muslim/RakibActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BayzidActivity extends AppCompatActivity {
+public class RakibActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bayzid);
+        setContentView(R.layout.activity_rakib);
     }
 }
